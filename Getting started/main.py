95278ce4bce96512621ee1c14ac7c3f240d76361
Damien Wohwe Sambo <wsdamieno@gmail.com>
2022-06-20 13:43:11 +0100

    getting started with the BLE scan

---
+++ Getting started/main.py
@@ -0,0 +1,21 @@
+import ubinascii
+import pycom
+
+from network import Bluetooth
+
+bluetooth = Bluetooth()
+bluetooth.start_scan(20) # the value -1 is reserve to scan without any timeout
+
+pycom.rgbled(False)
+
+while bluetooth.isscanning():
+    adv = bluetooth.get_adv()
+    if adv:
+        # try to get the complete name
+        print(bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL))
+
+        mfg_data = bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_MANUFACTURER_DATA)
+
+        if mfg_data:
+            # try to get the manufacturer data (Apple's iBeacon data is sent here)
+            print(ubinascii.hexlify(mfg_data))
